3363161628fa700caeb9bb034bb190ebf26d4d56
Xuat <phanxuat2003@gmail.com>
2023-01-20 22:19:38 +0700

    ok

--- js/slide-show.js
+++ js/slide-show.js
@@ -1,6 +1,6 @@
 var index = 1;
 changeImage = function() {
-    var imgs = ["/images/anh-thuc-te-1.jpg", "/images/anh-thuc-te.jpg", "/images/anh-thuc-te-2.jpg"];
+    var imgs = ["./images/anh-thuc-te-1.jpg", "./images/anh-thuc-te.jpg", "./images/anh-thuc-te-2.jpg"];
     document.getElementById("slide-show").src = imgs[index];
     index++;
     if (index == 3) {
